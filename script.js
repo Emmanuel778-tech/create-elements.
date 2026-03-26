9d1e3df82af1acc974223c93541185712b499e41
Emmanuel778-tech <eemmanuelcastro12@gmail.com>
2026-03-26 16:27:47 +0000

    progresws

---
+++ script.js
@@ -0,0 +1,22 @@
+console.log("script started");
+
+function start(){
+    console.log("click");
+
+    //create an H1 heading
+    let heading = document.createElement("h1");
+
+    //setup text and styles
+    heading.innerText = " Attendence";
+    heading.style.color = "red";
+    heading.style.textDecoration = "underLine";
+
+    //Add the h1 to the page
+    document.body.appendChild(heading);
+
+    let addName = readLine""
+}
+
+function addName(){
+
+}
